408c320774f4c4f6db1efe267e09ac7f06a7ea6e
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-14 15:45:00 -0500

    Adds solution for well of ideas.

---
+++ well_of_ideas.py
@@ -0,0 +1,9 @@
+#easy
+def well(x):
+    good_ideas = x.count('good')
+    if good_ideas > 2:
+        return "I smell a series!"
+    elif good_ideas == 0:
+        return "Fail!"
+    else:
+        return "Publish!"
