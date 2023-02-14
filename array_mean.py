17f712ab6925717584321b132d75ec845d24537c
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-14 13:29:06 -0500

    Completes array mean solution.

---
+++ array_mean.py
@@ -0,0 +1,16 @@
+# easy
+
+def find_average(nums):
+    # tests try an empty list so need to account for division by 0
+    length = len(nums)
+    return sum(nums) / length if length > 0 else 0
+    
+    
+    # or if you don't wanna use sum, ternary
+    if length > 0:
+        sum = 0
+        for num in nums:
+            sum += num
+        return sum / len(nums)
+    else:
+        return 0
