15df919bec14778c69d9933ed18513cbc9793d73
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-14 13:23:20 -0500

    Completes sum of personalized message solution.

---
+++ personalized_message.py
@@ -0,0 +1,12 @@
+# easy
+
+# if you wanna go the traditional route:
+def greet(name, owner):
+    if name == owner:
+        return "Hello boss"
+    else:
+        return "Hello guest"
+
+# if you wanna hit 'em with the one-liner:
+def greet(name, owner):
+    return "Hello boss" if name == owner else "Hello guest"
