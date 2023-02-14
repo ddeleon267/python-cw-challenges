74854cb05b5c5f602920b6498e5749de162d8192
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-14 13:27:09 -0500

    Completes personalized string solution.

---
+++ combine_strings.py
@@ -0,0 +1,13 @@
+# easy
+
+# f string seems to be most modern way
+def combine_names(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+# more old school
+def combine_names(first, last):
+    return first + " " + last
+
+# super old school
+def combine_names(first, second):
+    return '{} {}'.format(first, second)
