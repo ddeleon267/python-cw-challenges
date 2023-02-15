2f3d3604ef8ae30efd05576dd66118f3ca979fad
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-15 12:03:29 -0500

    Minor comment updates

--- sum_mixed_array.py
+++ sum_mixed_array.py
@@ -1,14 +1,12 @@
-#fairly simple
-
 # if you wanna loop
-def sum_mix(arr):
+def sum_mix(list):
     my_sum = 0
-    for maybe_num in arr:
+    for maybe_num in list:
         my_sum += int(maybe_num) 
         # could check if it's an int already but not sure it makes much of a difference performance-wise
     return my_sum
 
 # if you wanna get fancy and use built-ins
-def sum_mix(arr):
-    return sum(map(int,arr))
+def sum_mix(list):
+    return sum(map(int,list))
     #map -> takes args of a function and an iterable, returns an iterable
