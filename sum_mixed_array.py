d35daec3c4995a71541fd968936acbf6b98d930a
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-14 21:42:14 -0500

    updates solution

--- sum_mixed_array.py
+++ sum_mixed_array.py
@@ -1,9 +1,11 @@
 #fairly simple
 
+# if you wanna loop
 def sum_mix(arr):
     my_sum = 0
     for maybe_num in arr:
-        my_sum += int(maybe_num)
+        my_sum += int(maybe_num) 
+        # could check if it's an int already but not sure it makes much of a difference performance-wise
     return my_sum
 
 # if you wanna get fancy and use built-ins
