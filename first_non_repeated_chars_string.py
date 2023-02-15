2b6edc07e6a535395915e68337be7883d9ff118f
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-15 11:58:17 -0500

    Minor edits

--- first_non_repeated_chars_string.py
+++ first_non_repeated_chars_string.py
@@ -1,4 +1,4 @@
-def first_non_repeated(s):
-    for char in s:
-        if s.count(char) == 1: return char 
-    #fns return None by default in python, no final return needed
+def first_non_repeated(string):
+    for char in string:
+        if string.count(char) == 1: return char 
+    #fns return None by default in python, no final return needed for non-match
