436ba02cf73681bf5cac5c7d7112212a05cd6b14
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-15 11:59:07 -0500

    Removes ruby references

--- sort_and_star.py
+++ sort_and_star.py
@@ -1,16 +1,17 @@
-# the directions for this one make my head hurt, but essentially:
-# sort array alpabetically
-# grab first element
-# return element with *** between each letter
+# the codewars directions for this one make my head hurt, but essentially:
+# sort list alpabetically
+# grab first element from list
+# return element as a string, with *** between each letter
 
 #verbose
-def two_sort(array):
-    sorted_arr = sorted(arr)
-    chosen_word = sorted_arr[0]
+def two_sort(list):
+    sorted_list = sorted(list)
+    chosen_word = sorted_list[0]
     return "***".join(chosen_word) 
     # strings are iterables in python, can call join on them directly
 
 # one-liner
-def two_sort(arr):
-    return '***'.join(sorted(arr)[0])
+def two_sort(list):
+    return '***'.join(sorted(list)[0])
     # or can use min() instead of sorted if you wanna get fancy
+    #
